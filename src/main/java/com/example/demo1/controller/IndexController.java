13a811e1e5a6647ff66078c0f2bcdacf1f7ac3cb
zjucsq <1064359985@qq.com>
2020-04-25 00:20:29 +0800

    fileUpload

---
+++ src/main/java/com/example/demo1/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.demo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "index";
+    }
+
+}
